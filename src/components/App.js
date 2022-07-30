523925cac6e7389382617a81c714115be4f0af72
ted <82011813+unnotedme@users.noreply.github.com>
2022-07-30 01:14:09 -0300

    :sparkles: update profile

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,7 @@ import Dashboard from "./Dashboard";
 import Login from "./Login";
 import PrivateRoute from "./PrivateRoute";
 import ForgotPassword from "./ForgotPassword";
+import UpdateProfile from "./UpdateProfile";
 
 function App() {
   return (
@@ -18,6 +19,7 @@ function App() {
             <AuthProvider>
               <Routes>
                 <PrivateRoute exact path="/" component={Dashboard}/>
+                <PrivateRoute path="/update-profile" component={UpdateProfile}/>
                 <Route path="/signup" element={<Signup />} />
                 <Route path="/login" element={<Login />} />
                 <Route path="/forgot-password" element={<ForgotPassword />} />
