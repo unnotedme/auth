c0be64abeeeae35a1497011a4d209ba26b2c784a
ted <82011813+unnotedme@users.noreply.github.com>
2022-07-30 00:42:50 -0300

    :sparkles: log in, sign up and dashboard

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,28 @@
 import React from "react";
 import { Container } from "react-bootstrap";
+import { AuthProvider } from "../context/AuthContext";
 import Signup from "./Signup";
+import { HashRouter as Router, Routes, Route} from "react-router-dom";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
 
 function App() {
   return (
-    <Container className="d-flex align-items-center justify-content-center"
-      style={{ minHeight: "100vh" }}
-    >
-      <div className="w-100" style={{ maxWidth: "400px" }}>
-      <Signup />
-      </div>
-    </Container>
+      <Container className="d-flex align-items-center justify-content-center"
+        style={{ minHeight: "100vh" }}
+      >
+        <div className="w-100" style={{ maxWidth: "400px" }}>
+          <Router>
+            <AuthProvider>
+              <Routes>
+                <Route exact path="/" component={Dashboard}/>
+                <Route path="/signup" element={<Signup />} />
+                <Route path="/login" element={<Login />} />
+              </Routes>
+            </AuthProvider>
+          </Router>
+        </div>
+      </Container>
   ) 
 }
 
--- src/components/Login.js
+++ src/components/Login.js
@@ -1,14 +1,13 @@
 import React, { useRef, useState }from 'react'
 import { Card, Form, Button, Alert } from 'react-bootstrap'
-import { Link, useNavigate } from 'react-router-dom'
 import { useAuth } from '../context/AuthContext'
+import { Link, useNavigate } from 'react-router-dom'
 
-function Signup() {
+function Login() {
 
     const emailRef = useRef()
     const passwordRef = useRef()
-    const passwordConfirmRef = useRef()
-    const { signup } = useAuth()
+    const { login } = useAuth()
     const [error, setError] = useState('')
     const [loading, setLoading] = useState(false)
     const history = useNavigate()
@@ -16,17 +15,13 @@ function Signup() {
     async function handleSubmit(e) {
         e.preventDefault()
 
-        if (passwordRef.current.value !== passwordConfirmRef.current.value) {
-            return setError('Passwords do not match')
-        }
-
         try {
             setError('')
             setLoading(true)
-            await signup(emailRef.current.value, passwordRef.current.value)
+            await login(emailRef.current.value, passwordRef.current.value)
             history.push('/')
         } catch {
-            setError('Failed to create an account')
+            setError('Failed to log in')
         }
         setLoading(false)
     }
@@ -35,7 +30,7 @@ function Signup() {
     <>
         <Card>
             <Card.Body>
-                <h2 className="text-center mb-4">Sign Up</h2>
+                <h2 className="text-center mb-4">Log In</h2>
                 {error && <Alert variant="danger">{error}</Alert>}
                 <Form onSubmit={handleSubmit}>
                     <Form.Group id="email">
@@ -46,22 +41,18 @@ function Signup() {
                         <Form.Label>Password</Form.Label>
                         <Form.Control type="password" ref={passwordRef} required />
                     </Form.Group>
-                    <Form.Group id="password-confirm">
-                        <Form.Label>Password Confirmation</Form.Label>
-                        <Form.Control type="password" ref={passwordConfirmRef} required />
-                    </Form.Group>
                     <Button disabled={loading} className="w-100" type="submit">
-                        Sign Up
+                        Log In
                     </Button>
                 </Form>
             </Card.Body>
         </Card>
         <div className='w-100 text-center mt-2'>
-            Already have an account? <Link to='/login'>Log In</Link>
+            Need an account? <Link to='/signup'>Sign Up</Link>
         </div>
 
     </>
   )
 }
 
-export default Signup
+export default Login
